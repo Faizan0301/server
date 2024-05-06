01ea22464cde8140c5e15833599084b7fc615c29
Faizan0301 <faizanrednwhite@gmail.com>
2024-05-06 23:36:51 +0530

    server

---
+++ index.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const http = require('http')
+const port = "8080"
+
+const reqHndler = (req, res) => {
+    let fileName = ""
+    switch (req.url) {
+        case '/':
+            fileName = './home.html'
+            break;
+        case '/about':
+            fileName = './about.html'
+            break;
+        case '/contact':
+            fileName = './contact.html'
+            break;
+
+        default:
+            fileName = './error.html'
+    }
+    fs.readFile(fileName, (err, result) => {
+        if (!err) {
+            res.end(result);
+        }
+    })
+}
+const seraver = http.createServer(reqHndler)
+seraver.listen(port, (err) => {
+    if (err) {
+        console.log("srver not start in port.");
+        return false
+    }
+    console.log("server start in port" + port);
+})
